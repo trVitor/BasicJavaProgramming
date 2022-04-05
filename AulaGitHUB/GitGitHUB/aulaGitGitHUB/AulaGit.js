dd9d7f2ed2d56b5ea1517d86125ee04de2410310
Vitor Tadeu Rocha <102200168+trVitor@users.noreply.github.com>
2022-04-05 20:51:30 -0300

    Add files via upload

---
+++ AulaGitHUB/GitGitHUB/aulaGitGitHUB/AulaGit.js
@@ -0,0 +1,7 @@
+function soma() {
+var n1, n2;
+n1 = parseInt(document.getElementById("primeiroNumero").value);
+n2 = parseInt(document.getElementById("segundoNumero").value);
+soma = n1 + n2;
+document.getElementById("resposta").innerHTML = soma;
+}
