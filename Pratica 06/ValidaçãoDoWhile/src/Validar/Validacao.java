e5361f45e60fc820fa1f6f9d248d22f59e327342
Vitor Tadeu Rocha <102200168+trVitor@users.noreply.github.com>
2022-05-10 21:14:24 -0300

    Atividade Pratica 06
    Atividade Pratica utilizando Estrutura de Repetição DO WHILE

--- Pratica 06/ValidaçãoDoWhile/src/Validar/Validacao.java
+++ Pratica 06/ValidaçãoDoWhile/src/Validar/Validacao.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Validacao {
 
 	public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
+         try (Scanner ler = new Scanner(System.in)){
 		int confirmacao=0;
 		double nota=0, salario=0;
 		char sexo;
@@ -13,31 +13,31 @@ public class Validacao {
 		System.out.println("Digite a sua nota: ");
 		nota=ler.nextDouble();
 		if(nota >=0 && nota <=10) {
-			System.out.println("Essa é uma nota válida!!!  Deseja tentar Novamente ? \n[1] SIM ou Prescione [2] para continuar.");
+			System.out.println("Essa Ã© uma nota vÃ¡lida!!!  Deseja tentar Novamente ? \n[1] SIM ou Prescione [2] para continuar.");
 		}else 
-			System.out.println("Essa nota nota não é válida. Deseja tentar Novamente ? [1] SIM [2] NÃO");
+			System.out.println("Essa nota nota nÃ£o Ã© vÃ¡lida. Deseja tentar Novamente ? [1] SIM [2] NÃƒO");
             confirmacao=ler.nextInt();		
 		}while(confirmacao == 1 );
 		
 		do {
-		System.out.println("Digite o valor do Salário:");
+		System.out.println("Digite o valor do SalÃ¡rio:");
 		salario= ler.nextDouble();
 		if(salario > 0) {
-			System.out.println("Essa salário é válido!  Deseja tentar Novamente ? \n[1] SIM ou Digite [2] para continuar.");
+			System.out.println("Essa salÃ¡rio Ã© vÃ¡lido!  Deseja tentar Novamente ? \n[1] SIM ou Digite [2] para continuar.");
 		}else
-			System.out.println("Esse salário não é válido. Deseja tentar Novamente ? [1] SIM [2] NÃO");
+			System.out.println("Esse salÃ¡rio nÃ£o Ã© vÃ¡lido. Deseja tentar Novamente ? [1] SIM [2] NÃƒO");
 		  confirmacao=ler.nextInt();		
 				}while(confirmacao == 1 );
 
 		do {
-			System.out.println("Digite o seu genêro: [M] Masculino [F] Feminino");
+			System.out.println("Digite o seu genÃªro: [M] Masculino [F] Feminino");
 			sexo=ler.next().toUpperCase().charAt(0);
 			if(sexo == 'M') {
-				System.out.println("Genêro Masculino. Deseja tentar Novamente ? \n[1] SIM ou Digite [2] para continuar." );
+				System.out.println("GenÃªro Masculino. Deseja tentar Novamente ? \n[1] SIM ou Digite [2] para continuar." );
 			}else if(sexo == 'F') {
-				System.out.println("Genêro Feminino.  Deseja tentar Novamente ? \n[1] SIM ou Digite [2] para continuar." );
+				System.out.println("GenÃªro Feminino.  Deseja tentar Novamente ? \n[1] SIM ou Digite [2] para continuar." );
 			}else 
-			System.out.println("Esse genêro não é válido. Deseja tentar Novamente ? [1] SIM [2] NÃO");
+			System.out.println("Esse genÃªro nÃ£o Ã© vÃ¡lido. Deseja tentar Novamente ? [1] SIM [2] NÃƒO");
 			  confirmacao=ler.nextInt();		
 					}while(confirmacao == 1 );
 		
@@ -45,16 +45,16 @@ public class Validacao {
 			System.out.println("Digite a sua idade: ");
 			idade=ler.nextInt();
 			if(idade > 0 && idade <150) {
-				System.out.println("Essa é uma idade válida !! ");
+				System.out.println("Essa Ã© uma idade vÃ¡lida !! ");
 			}else
-					System.out.println("Essa é uma idade válida !! ");
+					System.out.println("Essa Ã© uma idade vÃ¡lida !! ");
 		}while(confirmacao == 1);
 			
-		System.out.println("FIM DA VALIDAÇÃO, DESEJA COMEÇAR DENOVO ? [1] SIM [2] NÃO");
+		System.out.println("FIM DA VALIDAÃ‡ÃƒO, DESEJA COMEÃ‡AR DENOVO ? [1] SIM [2] NÃƒO");
 		confirmacao=ler.nextInt();
 		}while(confirmacao == 1 );
 		System.out.println("PROGRAMA FINALIZADOO !!!");
-
+	 }
 	}		
 	}
 
