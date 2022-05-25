7b8ca8b52659ba5c351283b9c2967d95a92efb9f
Vitor TR <102200168+trVitor@users.noreply.github.com>
2022-05-24 21:20:53 -0300

    Atividade 09

---
+++ Matriz5x5/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Matriz1 matriz = new Matriz1();
+
+		matriz.ler();
+		matriz.imprimir();
+		matriz.diagonalPrincipal();
+		matriz.diagonalSecundaria();
+		matriz.modificar();
+		matriz.imprimir();
+	}
+}
