8571f22885c3bdf90bdcb82ca07c62aecb2c96af
Vitor TR <102200168+trVitor@users.noreply.github.com>
2022-05-25 21:00:19 -0300

    Atividade Pratica 09
    Atividade Pratica com a utilização de vetores para a criação de matrizes.

---
+++ Pratica 09/Matriz/src/Matrizes/Principal.java
@@ -0,0 +1,18 @@
+package Matrizes;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Matrizes matriz1 = new Matrizes();
+		
+		matriz1.lerNumerosMatrizes();
+		matriz1.imprimirMatrix5x5();
+		matriz1.mostrarLinhaParColunaImpar();
+		matriz1.mostrarMatrizTransposta();
+		matriz1.matrizModificada();
+		matriz1.imprimirMatrix5x5();
+		
+	}
+
+}
